683e74640a1c545492830e3b9810cf48b924b6e2
Reed von Redwitz <reed+github@vonredwitz.com>
2023-08-15 15:30:44 +0200

    feat: ability to have a user defined index, with blogindex available at /blog

---
+++ tests/separate_index_fixture/local.config.ts
@@ -0,0 +1,12 @@
+import { BlogOptions } from "../../mod.ts";
+
+export default {
+  title: "Demo Blog",
+  navbarItems: {
+    Blog: "/blog",
+    Archive: "/archive",
+  },
+  rootPath: import.meta.url,
+  postsPerPage: 5,
+  useSeparateIndex: true,
+} as BlogOptions;
---
+++ tests/separate_index_fixture/posts/boring-post.md
@@ -0,0 +1,11 @@
+---
+title: "Single Tag"
+date: 2023-8-15
+author: Some Author
+---
+
+The blog isn't the primary purpose of this site.
+
+<!--more-->
+
+Some extra content, just in case.
