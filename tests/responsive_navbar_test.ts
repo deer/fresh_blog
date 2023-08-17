c6e8ca0883c7548cb316df264f71f34c32a4bf7a
Reed von Redwitz <reed+github@vonredwitz.com>
2023-08-17 09:20:27 +0200

    chore: add tests

---
+++ tests/responsive_navbar_test.ts
@@ -0,0 +1,70 @@
+import { assert, assertEquals } from "./test_deps.ts";
+import { withPage } from "./test_utils.ts";
+
+Deno.test({
+  name: "navbar test with large browser",
+  async fn(t) {
+    await withPage(
+      "./tests/responsive_navbar_fixture/main.ts",
+      async (page, address) => {
+        // Setup
+        await page.goto(`${address}`, { waitUntil: "load" });
+        const navbar = await page.waitForSelector("nav");
+        assert(navbar != null);
+
+        await t.step("Navbar should be visible", async () => {
+          const visible = await navbar.evaluate((nav) =>
+            nav.style.maxHeight !== "0px"
+          );
+          assertEquals(visible, true);
+        });
+
+        await t.step("Navbar items should be correct", async () => {
+          const items = await page.$$eval(
+            "nav > ul > li > a",
+            (elements) => elements.map((e) => e.textContent),
+          );
+          assertEquals(items, ["Home", "Archive", "About", "Contact", "Other"]);
+        });
+      },
+      { args: ["--window-size=1920,1080"] },
+    );
+  },
+});
+
+Deno.test({
+  name: "navbar test with small browser",
+  async fn(t) {
+    await withPage(
+      "./tests/responsive_navbar_fixture/main.ts",
+      async (page, address) => {
+        await page.goto(`${address}`, { waitUntil: "load" });
+        const navbar = await page.waitForSelector("nav");
+        assert(navbar != null);
+
+        await t.step("navbar items should be hidden initially", async () => {
+          const classes = await navbar.evaluate((nav) =>
+            Array.from(nav.classList)
+          );
+          const itemsHidden = classes.includes("max-h-0");
+
+          assertEquals(itemsHidden, true);
+        });
+
+        await t.step(
+          "navbar items should be visible after clicking hamburger menu",
+          async () => {
+            await page.click("label");
+            const itemsVisible = await page.$$eval(
+              "nav > ul > li > a",
+              (elements) =>
+                elements.every((e) => getComputedStyle(e).display !== "none"),
+            );
+            assertEquals(itemsVisible, true);
+          },
+        );
+      },
+      { args: ["--window-size=375,812"] },
+    );
+  },
+});
--- tests/test_deps.ts
+++ tests/test_deps.ts
@@ -1,6 +1,7 @@
 export {
   DOMParser,
   Element,
+  HTMLDocument,
 } from "https://deno.land/x/deno_dom@v0.1.38/deno-dom-wasm.ts";
 export {
   assert,
@@ -8,3 +9,14 @@ export {
   assertNotEquals,
   assertStringIncludes,
 } from "https://deno.land/std@0.195.0/assert/mod.ts";
+export {
+  default as puppeteer,
+  Page,
+} from "https://deno.land/x/puppeteer@16.2.0/mod.ts";
+export type {
+  ChromeArgOptions,
+} from "https://deno.land/x/puppeteer@16.2.0/src/deno/LaunchOptions.ts";
+export { delay } from "https://deno.land/std@0.195.0/async/delay.ts";
+export {
+  TextLineStream,
+} from "https://deno.land/std@0.195.0/streams/text_line_stream.ts";
