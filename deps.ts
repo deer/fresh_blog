622f2d21b3d7829b6ba151195e95cdc4d0d68af6
Reed von Redwitz <reed+github@vonredwitz.com>
2023-08-18 18:12:21 +0200

    feat: first version of an init script

--- deps.ts
+++ deps.ts
@@ -13,9 +13,11 @@ export {
 export { Head } from "https://raw.githubusercontent.com/deer/fresh/feat_plugin_islands/runtime.ts";
 export {
   dirname,
+  extname,
   fromFileUrl,
   join,
 } from "https://deno.land/std@0.195.0/path/mod.ts";
+export * as JSONC from "https://deno.land/std@0.195.0/jsonc/mod.ts";
 export { extract } from "https://deno.land/std@0.195.0/front_matter/yaml.ts";
 export { CSS, render } from "https://deno.land/x/gfm@0.2.5/mod.ts";
 export { load } from "https://deno.land/std@0.195.0/dotenv/mod.ts";
@@ -26,3 +28,4 @@ export type {
   PageObjectResponse,
   RichTextItemResponse,
 } from "https://deno.land/x/notion_sdk@v2.2.3/src/api-endpoints.ts";
+export { $ } from "https://deno.land/x/dax@0.34.0/mod.ts";
---
+++ tests/cli_test.ts
@@ -0,0 +1,51 @@
+import { $, assertEquals, assertStringIncludes } from "./test_deps.ts";
+import { startFreshServer } from "./test_utils.ts";
+
+Deno.test({
+  name: "happy path init",
+  async fn(t) {
+    const tmpDirName = await Deno.makeTempDir();
+
+    await t.step("execute init command", async () => {
+      const cliProcess = new Deno.Command(Deno.execPath(), {
+        args: [
+          "run",
+          "-A",
+          "https://fresh.deno.dev/",
+          tmpDirName,
+        ],
+        stdin: "null",
+        stdout: "null",
+      });
+      const { code } = await cliProcess.output();
+      assertEquals(code, 0);
+    });
+
+    await t.step("init git repo", async () => {
+      await $`cd ${tmpDirName} && git init`;
+      await $`cd ${tmpDirName} && git add . && git commit -m "Initial commit"`;
+    });
+
+    await t.step("run blog init", async () => {
+      const homeDir = Deno.env.get("HOME");
+      const initScriptPath = `${homeDir}/code/fresh_blog/src/init.ts`;
+      await $`cd ${tmpDirName} && deno run -A ${initScriptPath}`.text();
+    });
+
+    await t.step("start blog and verify basics", async () => {
+      const { serverProcess, lines, address } = await startFreshServer({
+        args: ["run", "-A", "dev.ts"],
+        cwd: tmpDirName,
+      });
+
+      const resp = await fetch(address);
+      const body = await resp.text();
+      assertEquals(resp.status, 200);
+      assertStringIncludes(body, "No posts found. Start writing!");
+
+      await lines.cancel();
+      serverProcess.kill("SIGTERM");
+      await serverProcess.status;
+    });
+  },
+});
